85a878bf453cea03daecf3e4f2d783a36a5f7c7e
sozohoy <sozohoy@gmail.com>
2022-09-08 21:37:31 +0900

    style: 폴더 구조 정리 및 MVC 패턴 적용

---
+++ SuGo/SuGo/Controller/HomeController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SuGo/SuGo/Model/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/09/08.
+//
+
+import Foundation

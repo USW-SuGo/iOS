b8d094e9838dbcac9087d3c7d5e24fdb9002ec2c
sozohoy <sozohoy@gmail.com>
2022-09-14 19:38:33 +0900

    feat: add navigation bar

--- SuGo/SuGo/Controller/HomeController.swift
+++ SuGo/SuGo/Controller/HomeController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        addNavigationBar()
     }
     
     //MARK: Button Action
@@ -28,20 +29,30 @@ class ViewController: UIViewController {
         // Get Safe Area
         var statusBarHeight: CGFloat = 0
         statusBarHeight = UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0
-        
+
+        // navigationBar
+        let naviBar = UINavigationBar(frame: .init(x: 0, y: statusBarHeight, width: view.frame.width, height: statusBarHeight))
+        naviBar.isTranslucent = false
+        naviBar.backgroundColor = .systemBackground
+
+        let naviItem = UINavigationItem(title: "title")
+        naviItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        naviBar.items = [naviItem]
+
+        view.addSubview(naviBar)
         // NavigationBar
-        let customNavigationBar = UINavigationBar(frame: .init(x: 0,
-                                                         y: statusBarHeight,
-                                                         width: view.frame.width,
-                                                         height: statusBarHeight))
-        customNavigationBar.isTranslucent = false
-        customNavigationBar.backgroundColor = .systemBackground
-        
-        let customNavigationItem = UINavigationItem(title: "title")
-        customNavigationItem.rightBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem: .add,
-            target: self,
-            action: nil)
+//        let customNavigationBar = UINavigationBar(frame: .init(x: 0,
+//                                                         y: statusBarHeight,
+//                                                         width: view.frame.width,
+//                                                         height: statusBarHeight))
+//        customNavigationBar.isTranslucent = false
+//        customNavigationBar.backgroundColor = .systemBackground
+//
+//        let customNavigationItem = UINavigationItem(title: "title")
+//        customNavigationItem.rightBarButtonItem = UIBarButtonItem(
+//            barButtonSystemItem: .add,
+//            target: self,
+//            action: nil)
         
         
         

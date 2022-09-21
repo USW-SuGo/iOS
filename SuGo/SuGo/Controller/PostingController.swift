d66fc1fef1650770682cd6fc7570c308953eaee7
sozohoy <sozohoy@gmail.com>
2022-09-21 19:06:00 +0900

    feat: PostingVC design update

--- SuGo/SuGo/Controller/PostingController.swift
+++ SuGo/SuGo/Controller/PostingController.swift
@@ -9,21 +9,43 @@ import UIKit
 
 class PostingController: UIViewController {
 
+    //MARK: IBOutlets
+    
+    @IBOutlet weak var sugoButton: UIButton!
+    @IBOutlet weak var placeButton: UIButton!
+    
+    //MARK: Properties
+    
+    //MARK: Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        designButtons()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: Button Actions
+    
+    @IBAction func closeButtonClicked(_ sender: Any) {
+        self.dismiss(animated: true)
     }
-    */
+    
+    @IBAction func sugoButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func placeButtonClicked(_ sender: Any) {
+    }
+    
+    //MARK: Design Functions
+    
+    private func designButtons() {
+        
+        sugoButton.layer.cornerRadius = 12.0
+        sugoButton.layer.borderColor = UIColor.white.cgColor
+        
+        placeButton.layer.cornerRadius = 12.0
+        placeButton.layer.borderColor = UIColor.white.cgColor
 
+    }
+    
 }

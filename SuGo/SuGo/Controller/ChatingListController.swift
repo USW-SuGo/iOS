ac04f0c2a12d05d2d90cf77fcfc3f33bee3d7874
sozohoy <sozohoy@gmail.com>
2022-09-21 19:30:22 +0900

    add: ChatingListView & ChatingListController

---
+++ SuGo/SuGo/Controller/ChatingListController.swift
@@ -0,0 +1,31 @@
+//
+//  ChatingListController.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/09/21.
+//
+
+import UIKit
+
+class ChatingListController: UIViewController {
+
+    //MARK: IBOutlets
+    
+    //MARK: Properties
+    
+    //MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: Button Actions
+    
+    //MARK: Design Functions
+    
+    
+
+   
+}

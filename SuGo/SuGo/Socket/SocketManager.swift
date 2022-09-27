7359084bcf7be284158fc39c16c0c59f6bc0165c
sozohoy <sozohoy@gmail.com>
2022-09-27 20:42:17 +0900

    add: ChatingRoomController, SocketManager

---
+++ SuGo/SuGo/Socket/SocketManager.swift
@@ -0,0 +1,33 @@
+//
+//  SocketManager.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/09/27.
+//
+
+import Foundation
+import UIKit
+import SocketIO
+
+class SocketIOManager: NSObject {
+    
+    static let shared = SocketIOManager()
+    var manager: SocketManager
+    var socket: SocketIOClient
+    override init() {
+        super.init()
+        socket = self.manager.socket(forNamespace: "")
+        
+        socket.on("test") { dataArray, ack in
+            print("test")
+        }
+    }
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection
+    
+    
+}

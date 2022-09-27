26fb0640d381575fce07574a905fd3ed3305c11b
sozohoy <sozohoy@gmail.com>
2022-09-27 20:51:33 +0900

    feat: SocketIO Protocol

--- SuGo/SuGo/Socket/SocketManager.swift
+++ SuGo/SuGo/Socket/SocketManager.swift
@@ -9,31 +9,32 @@ import Foundation
 import UIKit
 import SocketIO
 
-class SocketIOManager: NSObject {
-    
-    static let shared = SocketIOManager()
-    var manager: SocketManager
-    var socket: SocketIOClient
-    override init() {
-        super.init()
-        socket = self.manager.socket(forNamespace: "")
-        
-        socket.on("test") { dataArray, ack in
-            print("test")
-        }
-    }
-    
-    func establishConnection() {
-        socket.connect()
-    }
-    
-    func closeConnection() {
-        socket.disconnect()
-    }
-    
-    func sendMessage(message: String, id: Int) {
-        // send contents
-    }
-    
-    
-}
+//class SocketIOManager: NSObject {
+//    
+//    static let shared = SocketIOManager()
+//    var manager: SocketManager
+//    var socket: SocketIOClient
+//
+////    override init() {
+////        self.init()
+////        socket = self.manager.socket(forNamespace: "")
+////
+////        socket.on("test") { dataArray, ack in
+////            print("test")
+////        }
+////    }
+//
+//    func establishConnection() {
+//        socket.connect()
+//    }
+//
+//    func closeConnection() {
+//        socket.disconnect()
+//    }
+//
+//    func sendMessage(message: String, id: Int) {
+//        // send contents
+//    }
+//
+//
+//}

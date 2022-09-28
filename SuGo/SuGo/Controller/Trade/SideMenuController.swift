b291d11f06d965fef4e933da65eb61eef8be762e
sozohoy <sozohoy@gmail.com>
2022-09-28 22:10:47 +0900

    add: sideMenu Controller

---
+++ SuGo/SuGo/Controller/Trade/SideMenuController.swift
@@ -0,0 +1,19 @@
+//
+//  SideMenuController.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/09/28.
+//
+
+import UIKit
+import SideMenu
+
+class SideMenuController: SideMenuNavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

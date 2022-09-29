9fdff2719d73cb96ecc258d3c51403600d1a8ced
sozohoy <sozohoy@gmail.com>
2022-09-29 20:51:23 +0900

    add: Home(Model)

---
+++ SuGo/SuGo/Model/Trade/Home.swift
@@ -0,0 +1,34 @@
+//
+//  Home.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/09/29.
+//
+
+import Foundation
+import AlamofireImage
+
+struct Home: Codable {
+    
+    let id: Int
+    let imageLink: [Image]
+    let contactPlace: String
+    let updatedAt: String
+    let title: String
+    let price: Int
+    let nickname: String
+    let category: String
+    
+    enum CodingKeys : String, CodingKey {
+        
+        case id
+        case imageLink
+        case contactPlace
+        case updatedAt
+        case title
+        case price
+        case nickname
+        case category
+        
+    }
+}

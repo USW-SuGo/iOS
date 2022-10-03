47d6adf09d8a25ddd395c92cdedec091e9ced291
sozohoy <sozohoy@gmail.com>
2022-10-03 18:05:38 +0900

    fix: codablefile 주석처리

--- SuGo/SuGo/Model/Trade/Home.swift
+++ SuGo/SuGo/Model/Trade/Home.swift
@@ -6,29 +6,31 @@
 //
 
 import Foundation
+import UIKit
+
+import Alamofire
 import AlamofireImage
 
-struct Home: Codable {
-    
-    let id: Int
-    let imageLink: [Image]
-    let contactPlace: String
-    let updatedAt: String
-    let title: String
-    let price: Int
-    let nickname: String
-    let category: String
-    
-    enum CodingKeys : String, CodingKey {
-        
-        case id
-        case imageLink
-        case contactPlace
-        case updatedAt
-        case title
-        case price
-        case nickname
-        case category
-        
-    }
-}
+//struct Home: Codable {
+//    
+//    let id: Int
+//    let imageLink: [Image]
+//    let contactPlace: String
+//    let updatedAt: String
+//    let title: String
+//    let price: Int
+//    let nickname: String
+//    let category: String
+//    
+//    enum CodingKeys : String, CodingKey {
+//        case id
+//        case imageLink
+//        case contactPlace
+//        case updatedAt
+//        case title
+//        case price
+//        case nickname
+//        case category
+//    }
+//
+//}

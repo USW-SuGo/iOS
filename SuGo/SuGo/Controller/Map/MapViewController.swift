fd93aba96351e8bccbd05e0914633bd44b105ccf
sozohoy <sozohoy@gmail.com>
2022-10-05 19:08:56 +0900

    add: MapViewController

---
+++ SuGo/SuGo/Controller/Map/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/10/05.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    //MARK: IBOutlets
+    
+    @IBOutlet weak var mapImageView: UIImageView!
+    
+    //MARK: Properties
+    
+    //MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: Button Actions
+    
+    //MARK: Design Functions
+
+
+}

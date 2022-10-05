842571d7fd4cfa5c31814bc5e73ee985834559db
sozohoy <sozohoy@gmail.com>
2022-10-05 21:02:49 +0900

    feat: 지도 확대-축소 최소범위 ~ 최대범위 지정 진행중

--- SuGo/SuGo/Controller/Map/MapViewController.swift
+++ SuGo/SuGo/Controller/Map/MapViewController.swift
@@ -21,10 +21,13 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         pinchAction()
         dragAction()
+        print("UIScreen Width & Height : \(UIScreen.main.bounds.width), \(UIScreen.main.bounds.height)")
+        print("center X , Y : \(mapImageView.center.x), \(mapImageView.center.y)")
         // Do any additional setup after loading the view.
     }
     
     private func pinchAction() {
+        
         let pinch = UIPinchGestureRecognizer(target: self, action: #selector(doPinch(_:)))
         self.view.addGestureRecognizer(pinch)
         
@@ -33,22 +36,23 @@ class MapViewController: UIViewController {
     @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
         let moveWidth = pinch.scale * self.mapImageView.frame.width
         let moveHeight = pinch.scale * self.mapImageView.frame.height
+        
+        // 핸드폰 기종 최소 비율로 축소 - > 원본
         if UIScreen.main.bounds.width > moveWidth || UIScreen.main.bounds.height > moveHeight {
+        
+            pinch.scale = 1
+            
+        // 핸드폰 화면 2.5배만큼 확대
+        } else if UIScreen.main.bounds.width * 2.5 < moveWidth || UIScreen.main.bounds.height * 2.5 < moveHeight{
             
             pinch.scale = 1
             
-//        } else if UIScreen.main.bounds.width * 3 < moveWidth || UIScreen.main.bounds.height * 3 < moveHeight {
-//
-//            pinch.scale = 1
-//
-        }
-        else {
+        // 즉 1 ~ 2.5배까지 +- 가능, 범주 내에 있을 시 확대 및 축소 진행
+        } else {
+            
             mapImageView.transform = mapImageView.transform.scaledBy(x: pinch.scale, y: pinch.scale)
+            pinch.scale = 1
         }
-        print("moveWidth & Height - \(moveWidth), \(moveHeight)")
-        print("pinchScale - \(pinch.scale)")
-        
-        pinch.scale = 1
     }
     
     private func dragAction() {
@@ -60,8 +64,21 @@ class MapViewController: UIViewController {
     @objc func doDrag(_ sender: UIPanGestureRecognizer) {
         
         let transition = sender.translation(in: mapImageView)
-        let changedX = mapImageView.center.x + transition.x
-        let changedY = mapImageView.center.y + transition.y
+        print("transition : \(transition)")
+        var changedX = mapImageView.center.x + transition.x
+        var changedY = mapImageView.center.y + transition.y
+        print("changedX & Y : \(changedX), \(changedY)")
+        
+        if (UIScreen.main.bounds.width / 2) - 100 > changedX  {
+            changedX = mapImageView.center.x
+            print("if 1")
+        }
+        
+        if (UIScreen.main.bounds.width / 2) + 100 < changedX {
+            changedX = mapImageView.center.x
+            print("if 2")
+        }
+        
         mapImageView.center = CGPoint(x: changedX, y: changedY)
         sender.setTranslation(CGPoint.zero, in: mapImageView)
         

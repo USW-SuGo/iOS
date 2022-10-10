cbb2bb0bfc63edc966f800371d4186b9de07b443
sozohoy <sozohoy@gmail.com>
2022-10-10 22:33:31 +0900

    feat: 이메일 정규식 추가

--- SuGo/SuGo/Model/Login/UserModel.swift
+++ SuGo/SuGo/Model/Login/UserModel.swift
@@ -13,17 +13,25 @@ struct LoginModel{
     // 2. 형식에 알맞을 시 해당 str post -> token check 하여 로그인
     
     func isValidId(id: String) -> Bool {
-            let IdRegEx = "[A-Za-z0-9]{5,13}"
-            let IdTest = NSPredicate(format: "SELF MATCHES %@", IdRegEx)
-            return IdTest.evaluate(with: id)
-        } // 아이디 형식 검사
+        let IdRegEx = "[A-Za-z0-9]{5,20}"
+        let IdTest = NSPredicate(format: "SELF MATCHES %@", IdRegEx)
+        
+        return IdTest.evaluate(with: id)
+    } // 아이디 형식 검사
         
         
     func isValidPassword(pwd: String) -> Bool {
-            let passwordRegEx = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,50}"
-            let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
-            return passwordTest.evaluate(with: pwd) // 비밀번호 형식 검사
+        let passwordRegEx = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,50}"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
         
+        return passwordTest.evaluate(with: pwd) // 비밀번호 형식 검사
+    }
+    
+    func isValidEmail(email: String) -> Bool {
+        let EmailRegEx = "[A-Za-z0-9]{5,13}"
+        let EmailTest = NSPredicate(format: "SELF MATCHES %@", EmailRegEx)
+        
+        return EmailTest.evaluate(with: email)
     }
     // "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,50}"
 }

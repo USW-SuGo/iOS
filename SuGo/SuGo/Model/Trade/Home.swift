215a7ce80bf0dc72b4c95a4e0757fb1cf10de696
sozohoy <sozohoy@gmail.com>
2022-10-12 21:53:17 +0900

    feat: 구조체 수정

--- SuGo/SuGo/Model/Trade/Home.swift
+++ SuGo/SuGo/Model/Trade/Home.swift
@@ -9,27 +9,17 @@ import Foundation
 import UIKit
 
 import Alamofire
+import SwiftyJSON
 
-struct Home: Codable {
+struct Home {
     
-    let id: Int
-    let imageLink: [String]
-    let contactPlace: String
-    let updatedAt: String
-    let title: String
-    let price: Int
-    let nickname: String
-    let category: String
-    
-    enum CodingKeys : String, CodingKey {
-        case id
-        case imageLink
-        case contactPlace
-        case updatedAt
-        case title
-        case price
-        case nickname
-        case category
-    }
+    var id: Int
+    var imageLink: [String]
+    var contactPlace: String
+    var updatedAt: String
+    var title: String
+    var price: Int
+    var nickname: String
+    var category: String
 
 }

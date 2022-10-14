d38858edf4836402ac088621e51db162e52df595
sozohoy <sozohoy@gmail.com>
2022-10-14 18:57:30 +0900

    refactor: struct home -> productContents(재사용 위함)

---
+++ SuGo/SuGo/Model/Trade/ProductContents.swift
@@ -0,0 +1,23 @@
+//
+//  Home.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/09/29.
+//
+
+import Foundation
+import UIKit
+
+
+struct ProductContents {
+    
+    var id: Int = 0
+    var imageLink: [String] = [""]
+    var contactPlace: String = ""
+    var updatedAt: String = ""
+    var title: String = ""
+    var price: String = ""
+    var nickname: String = ""
+    var category: String = ""
+
+}

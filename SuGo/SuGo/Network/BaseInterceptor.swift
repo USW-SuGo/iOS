9296789012efd773f6618d9b72b373bb41483a18
sozohoy <sozohoy@gmail.com>
2022-10-29 23:32:55 +0900

    feat: 토큰 인터셉터 구현

--- SuGo/SuGo/Network/BaseInterceptor.swift
+++ SuGo/SuGo/Network/BaseInterceptor.swift
@@ -8,18 +8,28 @@
 import Foundation
 
 import Alamofire
+import SwiftyJSON
+import KeychainSwift
 
 // it will be adapt & retry
 class BaseInterceptor: RequestInterceptor {
+    
+    let keychain = KeychainSwift()
     
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
         print("BaseInterceptor - adapt() called")
         
 //        var request = urlRequest
+        guard urlRequest.url?.absoluteString.hasPrefix(API.BASE_URL) == true, let accessToken = keychain.get("AccessToken") else {
+            completion(.success(urlRequest))
+            return
+        }
+        
         var request = urlRequest
         
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue(accessToken, forHTTPHeaderField: "Authorization")
         
         completion(.success(urlRequest))
         
@@ -27,9 +37,45 @@ class BaseInterceptor: RequestInterceptor {
     
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
         print("BaseInterceptor - retry() called")
+        guard let response = request.task?.response as? HTTPURLResponse, response.statusCode == 400 else {
+            print(".doNotRetry")
+            completion(.doNotRetryWithError(error))
+            return
+        }
         
-        completion(.doNotRetry)
+        print(".retry")
+        
+        let url = API.BASE_URL + "/token"
+        let headers: HTTPHeaders = [
+            "Authorization" : keychain.get("RefreshToken") ?? ""
+        ]
+        
+        AF.request(url, method: .post, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+            switch response.result {
+            case .success(_):
+                
+                // Token - Dictionary to String
+                let tokens = String((response.response?.headers.dictionary["Authorization"] ?? ""))
+                let splitToken = tokens.components(separatedBy: ",")
+                let refreshStartIndex = splitToken[0].index(splitToken[0].startIndex,
+                                                                offsetBy: 14)
+                let accessStartIndex = splitToken[1].index(splitToken[1].startIndex,
+                                                               offsetBy: 13)
+            
+                let refreshToken = String(splitToken[0][refreshStartIndex...])
+                var accessToken = String(splitToken[1].dropLast())
+                accessToken = String(accessToken[accessStartIndex...])
+            
+                // Keychain Setting
+            
+                self.keychain.clear()
+                self.keychain.set(accessToken, forKey: "AccessToken")
+                self.keychain.set(refreshToken, forKey: "RefreshToken")
+            
+            case .failure(_):
+                completion(.doNotRetryWithError(error))
+            }
+        }
     }
-    
 }
 

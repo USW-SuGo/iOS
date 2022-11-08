923b7d18f0557567009091747896437998564dbd
sozohoy <sozohoy@gmail.com>
2022-11-08 18:27:41 +0900

    add: LikePostingCell

---
+++ SuGo/SuGo/View/UserInfo/LikePostingCell.swift
@@ -0,0 +1,23 @@
+//
+//  LikePostingCell.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/11/08.
+//
+
+import UIKit
+
+class LikePostingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

cce6a4e4e5ca590ff394927006de8356f90c7c9e
sozohoy <sozohoy@gmail.com>
2022-11-10 18:02:31 +0900

    add: emailAuthVC, View 추가

---
+++ SuGo/SuGo/Controller/Login/EmailAuthController.swift
@@ -0,0 +1,18 @@
+//
+//  EmailAuthController.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/11/10.
+//
+
+import UIKit
+
+class EmailAuthController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}

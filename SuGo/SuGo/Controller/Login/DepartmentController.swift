06af43f843731e0cc4e8a66dd4394e165fc9887d
sozohoy <sozohoy@gmail.com>
2022-11-12 20:23:21 +0900

    feat: search -> section 사전순 정렬로 수정

--- SuGo/SuGo/Controller/Login/DepartmentController.swift
+++ SuGo/SuGo/Controller/Login/DepartmentController.swift
@@ -20,40 +20,52 @@ class DepartmentController: UIViewController {
   
   //MARK: Properties
     
-  let departments = ["국어국문학", "사학", "영어영문학", "러시아어문학", "일어일문학", "중어중문학", "법학", "행정학", "소방행정학과(야)", "경제금융학과", "국제개발협력학과", "경영학과", "회계학과", "호텔경영학과", "외식경영학과", "바이오공학 및 마케팅", "융합화학산업", "건설환경공학", "환경에너지공학", "건축학과", "도시부동산학과", "산업공학과", "기계공학과", "전자재료공학", "전기공학과", "전자공학과", "신소재공학과", "화학공학과", "컴퓨터SW학과", "미디어SW학과", "정보통신공학과", "정보보호학과", "간호학과", "아동가족복지학과", "의류학과", "식품영양학과", "체육학과", "레저스포츠학과", "운동건강관리학과", "회화과", "조소과", "커뮤니케이션디자인학과", "패션디자인학과", "공예디자인학과", "작곡과", "성악과", "피아노과", "관현악과", "국악과", "영화영상과", "연극과", "문화컨텐츠테크놀러지학과", "클라우드융복합학과", "시스템반도체융복합학과"]
+  let sections = ["ㄱ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅍ", "ㅎ"] // 12
+  
+  let departments = [["간호학과", "건설환경공학", "건축학과", "경영학과", "경제금융학과", "공예디자인학과", "관현악과", "국악과", "국어국문학", "국제개발협력학과", "기계공학과"],
+                     ["도시부동산학과"],
+                     ["러시아어문학", "레저스포츠학과"],
+                     ["문화컨텐츠테크놀러지학과", "미디어SW학과"],
+                     ["바이오공학 및 마케팅", "법학"],
+                     ["사학", "산업공학과", "성악과", "소방행정학과(야)", "시스템반도체융복합학과", "식품영양학과", "신소재공학과"],
+                     ["아동가족복지학과", "연극과", "영어영문학", "영화영상과", "외식경영학과", "운동건강관리학과", "융합화학산업", "의류학과", "일어일문학"],
+                     ["작곡과", "전기공학과", "전자공학과", "전자재료공학", "정보보호학과", "정보통신공학과", "조소과", "중어중문학"],
+                     ["체육학과"],
+                     ["커뮤니케이션디자인학과", "컴퓨터SW학과", "클라우드융복합학과"],
+                     ["패션디자인학과", "피아노과"],
+                     ["행정학", "호텔경영학과", "화학공학과", "환경에너지공학", "회계학과", "회화과"]]
     
   let userInfo = UserInfo.shared
   var searchDepartments: [String] = []
   let searchController = UISearchController(searchResultsController: nil)
   let colorLiteralGreen = #colorLiteral(red: 0.2208407819, green: 0.6479891539, blue: 0.4334517121, alpha: 1)
-  var searchMode: Bool {
-      return searchController.isActive && !searchController.searchBar.text!.isEmpty
-  }
+//  var searchMode: Bool {
+//      return searchController.isActive && !searchController.searchBar.text!.isEmpty
+//  }
   
   //MARK: Functions
   
   override func viewDidLoad() {
       super.viewDidLoad()
       designIBOutlets()
-      setUpSearchController()
       // Do any additional setup after loading the view.
   }
   
-  private func setUpSearchController() {
-      tableView.separatorStyle = .none
-      
-      tableView.tableHeaderView = searchController.searchBar
-      
-      searchController.searchBar.placeholder = "학과를 검색하세요."
-      // 사용자 검색 값에 따라 컨텐츠를 업데이트 시켜주는 프로퍼티
-      searchController.searchResultsUpdater = self
-      // 검색 시 기본 컨텐츠가 가려지는 여부
-      searchController.obscuresBackgroundDuringPresentation = false
-      
-      searchController.searchBar.searchBarStyle = .prominent
-      searchController.searchBar.showsCancelButton = false
-      searchController.searchBar.sizeToFit()
-  }
+//  private func setUpSearchController() {
+//      tableView.separatorStyle = .none
+//
+//      tableView.tableHeaderView = searchController.searchBar
+//
+//      searchController.searchBar.placeholder = "학과를 검색하세요."
+//      // 사용자 검색 값에 따라 컨텐츠를 업데이트 시켜주는 프로퍼티
+//      searchController.searchResultsUpdater = self
+//      // 검색 시 기본 컨텐츠가 가려지는 여부
+//      searchController.obscuresBackgroundDuringPresentation = false
+//
+//      searchController.searchBar.searchBarStyle = .prominent
+//      searchController.searchBar.showsCancelButton = false
+//      searchController.searchBar.sizeToFit()
+//  }
   
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         self.view.endEditing(true)
@@ -116,50 +128,124 @@ class DepartmentController: UIViewController {
 }
 
 extension DepartmentController: UITableViewDataSource, UITableViewDelegate {
-    
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return sections.count
+  }
+  
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return sections[section]
+  }
+  
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchMode ? searchDepartments.count : departments.count
+      switch section{
+      case 0:
+        return departments[0].count
+      case 1:
+        return departments[1].count
+      case 2:
+        return departments[2].count
+      case 3:
+        return departments[3].count
+      case 4:
+        return departments[4].count
+      case 5:
+        return departments[5].count
+      case 6:
+        return departments[6].count
+      case 7:
+        return departments[7].count
+      case 8:
+        return departments[8].count
+      case 9:
+        return departments[9].count
+      case 10:
+        return departments[10].count
+      case 11:
+        return departments[11].count
+      default:
+        return 0
+      }
+      // searchMode ? searchDepartments.count : departments.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "departmentCell",
+      let cell = tableView.dequeueReusableCell(withIdentifier: "departmentCell",
                                                  for: indexPath) as! DepartmentCell
+      switch indexPath.section{
+      case 0:
         cell.checkButton.image = UIImage(systemName: "circle")
-        cell.departmentLabel.text = searchMode ? searchDepartments[indexPath.row] : departments[indexPath.row]
+        cell.departmentLabel.text = departments[0][indexPath.row]
+      case 1:
+        cell.checkButton.image = UIImage(systemName: "circle")
+        cell.departmentLabel.text = departments[1][indexPath.row]
+      case 2:
+        cell.checkButton.image = UIImage(systemName: "circle")
+        cell.departmentLabel.text = departments[2][indexPath.row]
+      case 3:
+        cell.checkButton.image = UIImage(systemName: "circle")
+        cell.departmentLabel.text = departments[3][indexPath.row]
+      case 4:
+        cell.checkButton.image = UIImage(systemName: "circle")
+        cell.departmentLabel.text = departments[4][indexPath.row]
+      case 5:
+        cell.checkButton.image = UIImage(systemName: "circle")
+        cell.departmentLabel.text = departments[5][indexPath.row]
+      case 6:
+        cell.checkButton.image = UIImage(systemName: "circle")
+        cell.departmentLabel.text = departments[6][indexPath.row]
+      case 7:
+        cell.checkButton.image = UIImage(systemName: "circle")
+        cell.departmentLabel.text = departments[7][indexPath.row]
+      case 8:
+        cell.checkButton.image = UIImage(systemName: "circle")
+        cell.departmentLabel.text = departments[8][indexPath.row]
+      case 9:
+        cell.checkButton.image = UIImage(systemName: "circle")
+        cell.departmentLabel.text = departments[9][indexPath.row]
+      case 10:
+        cell.checkButton.image = UIImage(systemName: "circle")
+        cell.departmentLabel.text = departments[10][indexPath.row]
+      case 11:
+        cell.checkButton.image = UIImage(systemName: "circle")
+        cell.departmentLabel.text = departments[11][indexPath.row]
+
+      default:
+        break
+      }
+      
         
-        return cell
+      return cell
     }
     
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        userInfo.department = departments[indexPath.row]
-        showDepartmentLabel.text = "\(userInfo.loginId ?? "")님의 학과는 \(userInfo.department ?? "") 입니다 !"
+      userInfo.department = departments[indexPath.section][indexPath.row]
+      showDepartmentLabel.text = "\(userInfo.loginId ?? "")님의 학과는 \(userInfo.department ?? "") 입니다 !"
     }
 
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60.0
+      return 60.0
     }
     
 }
 
-extension DepartmentController: UISearchResultsUpdating {
-    
-    func filteredContentForSearchText(_ searchText: String) {
-        
-        searchDepartments = departments.filter({ (department) -> Bool in
-            return department.lowercased().contains(searchText.lowercased())
-        })
-        
-        tableView.reloadData()
-    }
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        filteredContentForSearchText(searchController.searchBar.text ?? "")
-    }
-    
-    
-}
+//extension DepartmentController: UISearchResultsUpdating {
+//
+//    func filteredContentForSearchText(_ searchText: String) {
+//
+//        searchDepartments = departments.filter({ (department) -> Bool in
+//            return department.lowercased().contains(searchText.lowercased())
+//        })
+//
+//        tableView.reloadData()
+//    }
+//
+//    func updateSearchResults(for searchController: UISearchController) {
+//        filteredContentForSearchText(searchController.searchBar.text ?? "")
+//    }
+//}
 
 class DepartmentCell: UITableViewCell {
     

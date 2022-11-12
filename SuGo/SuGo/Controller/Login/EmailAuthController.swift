0b5c4072436ecf3b08b280cb57769a21c624f13e
sozohoy <sozohoy@gmail.com>
2022-11-12 20:23:38 +0900

    feat: 회원가입 로직 적용 및 메소드명 수정

--- SuGo/SuGo/Controller/Login/EmailAuthController.swift
+++ SuGo/SuGo/Controller/Login/EmailAuthController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 import Alamofire
 import SwiftyJSON
-import WebKit
 
 class EmailAuthController: UIViewController {
 
@@ -21,8 +20,9 @@ class EmailAuthController: UIViewController {
   @IBOutlet weak var signUpButton: UIButton!
   
   //MARK: Properties
-  
+
   let colorLiteralGreen = #colorLiteral(red: 0.2208407819, green: 0.6479891539, blue: 0.4334517121, alpha: 1)
+  let userInfo = UserInfo.shared
   
   //MARK: Life Cycle
   
@@ -36,20 +36,40 @@ class EmailAuthController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     designIBOutlets()
-    }
+  }
   
   //MARK: Functions
   
   //MARK: Button Actions
   
   @IBAction func portalButtonClicked(_ sender: Any) {
-//    guard let url = URL(string: "https://naver.com") else { return }
-//    let request = URLRequest(url: url)
-//    webView.load(request)
+    let portalView = UIStoryboard(name: "PortalView", bundle: nil)
+    let portalController = portalView.instantiateViewController(withIdentifier: "portalVC")
+    present(portalController, animated: true)
+  }
+  
+  @IBAction func signUpButtonClicked(_ sender: Any) {
+    guard let userIndex = userInfo.userIndex else { return }
+    guard let payload = emailAuthTextField.text else { return }
+    AlamofireManager
+      .shared
+      .session
+      .request(LoginRouter.checkAuthNumber(userId: userIndex, payload: payload))
+      .validate()
+      .responseJSON { response in
+        
+        guard let statusCode = response.response?.statusCode, statusCode == 200 else {
+          self.customAlert(title: "인증번호가 일치하지 않아요!", message: "다시 입력해주세요!")
+          return
+        }
+        self.customAlert(title: "회원가입에 성공했어요!", message: "오늘도 수고하세요!")
+        self.presentingViewController?.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
+        
+      }
   }
   
   @IBAction func closeButtonClicked(_ sender: Any) {
-    customAlert(title: "회원가입을 종료하시겠어요?", message: "종료하게 될 경우, 한 시간 뒤에 동일 계정으로 가입이 가능합니다.")
+    customAlertWithHandler(title: "회원가입을 종료하시겠어요?", message: "종료하게 될 경우, 한 시간 뒤에 동일 계정으로 가입이 가능합니다.")
   }
   
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
@@ -70,7 +90,7 @@ class EmailAuthController: UIViewController {
     portalButton.layer.borderWidth = 0.5
   }
   
-  private func customAlert(title: String, message: String) {
+  private func customAlertWithHandler(title: String, message: String) {
     let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
     alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { UIAlertAction in
       self.presentingViewController?.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
@@ -78,5 +98,13 @@ class EmailAuthController: UIViewController {
     alert.addAction(UIAlertAction(title: "취소", style: .destructive))
     self.present(alert, animated: true, completion: nil)
   }
+  
+  private func customAlert(title: String, message: String) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "확인", style: .default))
+    self.present(alert, animated: true, completion: nil)
+  }
     
 }
+
+

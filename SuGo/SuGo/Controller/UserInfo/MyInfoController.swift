50eaa80c6a4de7fc3a240406e56248446986ad82
sozohoy <sozohoy@gmail.com>
2022-11-15 23:31:50 +0900

    feat: 내정보 기능 추가(수정, 삭제, 액션시트)

--- SuGo/SuGo/Controller/UserInfo/MyInfoController.swift
+++ SuGo/SuGo/Controller/UserInfo/MyInfoController.swift
@@ -41,13 +41,16 @@ class MyInfoController: UIViewController {
     
     //MARK: Properties
     
-    let keychain = KeychainSwift()
-    var myPage = MyPage()
-    var userPosting: [MyPagePosting] = []
-    var likePosting: [MyPagePosting] = []
-    var testUser = [1, 2, 3]
-    var testLike = [1, 2]
-    let colorLiteralGreen = #colorLiteral(red: 0.2208407819, green: 0.6479891539, blue: 0.4334517121, alpha: 1)
+  let keychain = KeychainSwift()
+  var myPage = MyPage()
+  var userPosting: [MyPagePosting] = []
+  var likePosting: [MyPagePosting] = []
+  var testUser = [1, 2, 3]
+  var testLike = [1, 2]
+  let colorLiteralGreen = #colorLiteral(red: 0.2208407819, green: 0.6479891539, blue: 0.4334517121, alpha: 1)
+  let modifyData = ModifyProduct.shared
+  // red: 0.2208407819, green: 0.6479891539, blue: 0.4334517121, alpha: 1
+  
     
     //MARK: Functions
     
@@ -61,7 +64,7 @@ class MyInfoController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         
         super.viewWillAppear(animated)
-        print("My Info")
+        print("My Info View Will Appear")
         
         userPosting.removeAll()
         getMyPage(page: 0, size: 10)
@@ -79,7 +82,7 @@ class MyInfoController: UIViewController {
                 
                 if response.response?.statusCode == 200 {
                     
-                    print(JSON(response.data))
+                    
                     
                     if self.myPage.userIndex == "" {
                         self.updateMyPage(json: JSON(response.data ?? ""))
@@ -115,9 +118,10 @@ class MyInfoController: UIViewController {
             
             let myPosting = json["myPosting"][i]
                 
-            let getData = MyPagePosting(userIndex: myPosting["id"].stringValue,
+            let getData = MyPagePosting(productIndex: myPosting["id"].intValue,
                                         title: myPosting["title"].stringValue,
                                         price: myPosting["price"].stringValue,
+                                        decimalWon: decimalWon(price: myPosting["price"].intValue),
                                         category: myPosting["category"].stringValue,
                                         status: myPosting["status"].boolValue,
                                         imageLink: myPosting["imageLink"].stringValue,
@@ -134,83 +138,147 @@ class MyInfoController: UIViewController {
 
     }
     
-    
+  private func deletePost(indexPath: IndexPath) {
+    AlamofireManager
+      .shared
+      .session
+      .request(PostRouter.deletePost(productPostId: userPosting[indexPath.row].productIndex))
+      .validate()
+      .responseJSON { response in
+        
+        self.userPosting.removeAll()
+        self.getMyPage(page: 0, size: 10)
+      }
+  }
     
     //MARK: Button Actions
     
-    @IBAction func myPostButtonClicked(_ sender: Any) {
+  @IBAction func myPostButtonClicked(_ sender: Any) {
+  
+      tableView.tag = 1
+      tableView.reloadData()
+      
+  }
+  
+  @IBAction func likePostButtonClicked(_ sender: Any) {
+      
+      tableView.tag = 2
+      tableView.reloadData()
+      
+  }
+  
+  @objc func modifyButtonClicked(sender: UIButton) {
+    let indexPath = IndexPath(row: sender.tag, section: 0)
+    let postingView = UIStoryboard(name: "PostingView", bundle: nil)
+    guard let postingNavigationController = postingView.instantiateViewController(withIdentifier: "postingNavigationVC") as? UINavigationController else { return }
+    postingNavigationController.modalPresentationStyle = .fullScreen
+    postingNavigationController.title = "게시글 수정"
+    modifyData.productPostId = userPosting[indexPath.row].productIndex
+    modifyData.title = userPosting[indexPath.row].title
+    modifyData.category = userPosting[indexPath.row].category
+    modifyData.price = userPosting[indexPath.row].price
+    print(userPosting[indexPath.row].productIndex)
+    self.present(postingNavigationController, animated: true)
+  }
+  
+  @objc func upPostButtonClicked(sender: UIButton) {
+    let indexPath = IndexPath(row: sender.tag, section: 0)
+  }
+
+  @objc func kebabMenuClicked(_ sender: UIButton) {
     
-        tableView.tag = 1
-        tableView.reloadData()
-        
+    let indexPath = IndexPath(row: sender.tag, section: 0)
+    print(indexPath)
+    let actionSheetController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+    let deleteAction = UIAlertAction(title: "게시글 삭제", style: .destructive) { alert in
+      self.customAlert(title: "게시글을 정말 삭제하시겠어요?", message: "", indexPath: indexPath)
     }
+    let cancelAction = UIAlertAction(title: "닫기", style: .cancel)
     
-    @IBAction func likePostButtonClicked(_ sender: Any) {
-        
-        tableView.tag = 2
-        tableView.reloadData()
-        
-    }
+    actionSheetController.addAction(deleteAction)
+    actionSheetController.addAction(cancelAction)
+    present(actionSheetController, animated: true)
+  }
     
     //MARK: Design Functions
+  
+  func decimalWon(price: Int) -> String {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .decimal
+    let result = numberFormatter.string(from: NSNumber(value: price))! + "원"
     
-    private func registerXib() {
-        
-        let userPostingXib = UINib(nibName: "UserPostingCell", bundle: nil)
-        tableView.register(userPostingXib, forCellReuseIdentifier: "userPostingCell")
-        
-        let likePostingXib = UINib(nibName: "LikePostingCell", bundle: nil)
-        tableView.register(likePostingXib, forCellReuseIdentifier: "likePostingCell")
-        
+    return result
+  }
+  
+  private func customAlert(title: String, message: String, indexPath: IndexPath) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let deleteAction = UIAlertAction(title: "삭제", style: .destructive) { alert in
+      self.deletePost(indexPath: indexPath)
     }
+    let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+    alert.addAction(deleteAction)
+    alert.addAction(cancelAction)
     
-    private func designLoginView() {
-        
-        tableView.isHidden = false
-        myPostButton.isHidden = false
-        likePostButton.isHidden = false
-        
-        guestLabel.text = ""
-        userImage.tintColor = colorLiteralGreen
-        
-        userNicknameLabel.text = "오늘도 수고하세요! \(myPage.userNickname)님!"
-        userNicknameLabel.textColor = .black
-        
-        userMannerGradeLabel.text = myPage.userMannerGrade
-        userMannerGradeLabel.textColor = colorLiteralGreen
-        
-        userEvaluationCountLabel.text = "\(myPage.userEvaluationCount)"
-        userEvaluationCountLabel.textColor = colorLiteralGreen
-        
-        userTradeCountLabel.text = "\(myPage.userTradeCount)"
-        userTradeCountLabel.textColor = colorLiteralGreen
-        
-    }
+    self.present(alert, animated: true, completion: nil)
+  }
     
-    private func designGuestView() {
-        
-        tableView.isHidden = true
-        myPostButton.isHidden = true
-        likePostButton.isHidden = true
-        
-        userImage.tintColor = .lightGray
-        
-        guestLabel.text = "로그인이 필요해요!"
-        
-        userNicknameLabel.text = "게스트 상태입니다."
-        userNicknameLabel.textColor = .darkGray
-        
-        userMannerGradeLabel.text = "-"
-        userMannerGradeLabel.textColor = .darkGray
-        
-        userEvaluationCountLabel.text = "-"
-        userEvaluationCountLabel.textColor = .darkGray
-        
-        userTradeCountLabel.text = "-"
-        userTradeCountLabel.textColor = .darkGray
-        
-    }
+  private func registerXib() {
+      
+    let userPostingXib = UINib(nibName: "UserPostingCell", bundle: nil)
+    tableView.register(userPostingXib, forCellReuseIdentifier: "userPostingCell")
     
+    let likePostingXib = UINib(nibName: "LikePostingCell", bundle: nil)
+    tableView.register(likePostingXib, forCellReuseIdentifier: "likePostingCell")
+    
+  }
+  
+  private func designLoginView() {
+      
+    tableView.isHidden = false
+    myPostButton.isHidden = false
+    likePostButton.isHidden = false
+    
+    guestLabel.text = ""
+    userImage.tintColor = colorLiteralGreen
+    
+    userNicknameLabel.text = "오늘도 수고하세요! \(myPage.userNickname)님!"
+    userNicknameLabel.textColor = .black
+    
+    userMannerGradeLabel.text = myPage.userMannerGrade
+    userMannerGradeLabel.textColor = colorLiteralGreen
+    
+    userEvaluationCountLabel.text = "\(myPage.userEvaluationCount)"
+    userEvaluationCountLabel.textColor = colorLiteralGreen
+    
+    userTradeCountLabel.text = "\(myPage.userTradeCount)"
+    userTradeCountLabel.textColor = colorLiteralGreen
+    
+  }
+  
+  private func designGuestView() {
+    
+    tableView.isHidden = true
+    myPostButton.isHidden = true
+    likePostButton.isHidden = true
+    
+    userImage.tintColor = .lightGray
+    
+    guestLabel.text = "로그인이 필요해요!"
+    
+    userNicknameLabel.text = "게스트 상태입니다."
+    userNicknameLabel.textColor = .darkGray
+    
+    userMannerGradeLabel.text = "-"
+    userMannerGradeLabel.textColor = .darkGray
+    
+    userEvaluationCountLabel.text = "-"
+    userEvaluationCountLabel.textColor = .darkGray
+    
+    userTradeCountLabel.text = "-"
+    userTradeCountLabel.textColor = .darkGray
+      
+  }
+  
 
 }
 
@@ -230,35 +298,44 @@ extension MyInfoController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if tableView.tag == 1 {
-                
-            let cell = tableView.dequeueReusableCell(withIdentifier: "userPostingCell",
+      if tableView.tag == 1 {
+              
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userPostingCell",
+                                                 for: indexPath) as! UserPostingCell
+        if userPosting.count > 0 { // indexPath out of range 방지 위함.
+          if let url = URL(string: userPosting[indexPath.row].imageLink) {
+              
+            cell.productImage.kf.indicatorType = .activity
+            cell.productImage.kf.setImage(with: url,
+                                   placeholder: nil,
+                                   options: [
+                                    .transition(.fade(0.1)),
+                                    .cacheOriginalImage
+                                        ],
+                                   progressBlock: nil)
             
-                                                     for: indexPath) as! UserPostingCell
-            
-            if let url = URL(string: userPosting[indexPath.row].imageLink) {
-                
-                cell.productImage.kf.indicatorType = .activity
-                cell.productImage.kf.setImage(with: url,
-                                       placeholder: nil,
-                                       options: [
-                                        .transition(.fade(0.1)),
-                                        .cacheOriginalImage
-                                            ],
-                                       progressBlock: nil)
-                
-                }
-            
-            cell.productImage.contentMode = .scaleAspectFill
-            cell.productImage.layer.cornerRadius = 6.0
-            cell.titleLabel.text = userPosting[indexPath.row].title
-            cell.nicknameLabel.text = "내가 쓴 글"
-            cell.placeUpdateCategoryLabel.text = "\(userPosting[indexPath.row].contactPlace) | \(userPosting[indexPath.row].updatedAt) | \(userPosting[indexPath.row].category)"
-            
-            
-            cell.selectionStyle = .none
-            
-            return cell
+          }
+          
+          cell.productImage.contentMode = .scaleAspectFill
+          cell.productImage.layer.cornerRadius = 6.0
+          cell.titleLabel.text = userPosting[indexPath.row].title
+          cell.nicknameLabel.text = "내가 쓴 글"
+          cell.placeUpdateCategoryLabel.text = "\(userPosting[indexPath.row].contactPlace) | \(userPosting[indexPath.row].updatedAt) | \(userPosting[indexPath.row].category)"
+          cell.priceLabel.text = userPosting[indexPath.row].decimalWon
+          cell.kebabMenuButton.tag = indexPath.row
+          cell.kebabMenuButton.addTarget(self,
+                                         action: #selector(kebabMenuClicked),
+                                         for: .touchUpInside)
+          cell.modifyButton.tag = indexPath.row
+          cell.modifyButton.addTarget(self,
+                                      action: #selector(modifyButtonClicked),
+                                      for: .touchUpInside)
+          
+          cell.selectionStyle = .none
+        }
+
+        
+        return cell
             
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "likePostingCell",

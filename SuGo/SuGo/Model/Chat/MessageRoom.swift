b5f4a2374e279fee1953e6e1d68cf28daa1fcd71
sozohoy <sozohoy@gmail.com>
2022-11-17 23:30:43 +0900

    add: 메세지 룸, 메세지 생성 모델 추가

---
+++ SuGo/SuGo/Model/Chat/MessageRoom.swift
@@ -0,0 +1,15 @@
+//
+//  MessageRoom.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/11/17.
+//
+
+import Foundation
+
+struct MessageRoom {
+  var myIndex: Int?
+  var opponentIndex: Int?
+  var message: String?
+  var messageTime: String?
+}

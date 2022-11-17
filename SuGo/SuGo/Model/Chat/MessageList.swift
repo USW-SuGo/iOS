726abeebb93cb23d90f37752f918c09ba55efe02
sozohoy <sozohoy@gmail.com>
2022-11-17 23:30:18 +0900

    add: 메세지 리스트 모델 추가

---
+++ SuGo/SuGo/Model/Chat/MessageList.swift
@@ -0,0 +1,19 @@
+//
+//  ChatList.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/11/15.
+//
+
+import Foundation
+
+struct MessageList {
+  var roomIndex: Int?
+  var oppositeIndex: Int?
+  var oppositeNickname: String?
+  var recentMessage: String?
+  var recentMessageTime: String?
+  var newMessageCount: Int?
+}
+
+

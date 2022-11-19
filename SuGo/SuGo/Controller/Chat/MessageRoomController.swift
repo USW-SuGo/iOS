d9d8bac27e2c0d0a9ebdebb948588e81786324d7
sozohoy <sozohoy@gmail.com>
2022-11-19 23:14:53 +0900

    feat: 메세지 방 스케치

---
+++ SuGo/SuGo/Controller/Chat/MessageRoomController.swift
@@ -0,0 +1,39 @@
+//
+//  ChatingRoomController.swift
+//  SuGo
+//
+//  Created by 한지석 on 2022/09/27.
+//
+
+import UIKit
+
+
+// senderId & receiverId 중 이전 페이지에서 상대방 index를 가져와
+// if sender이 상대방 index == 받은 메세지 else 내가 보낸 메세지
+// 
+
+
+class MessageRoomController: UIViewController {
+
+  //MARK: IBOutlets
+  
+  //MARK: Properties
+  
+  //MARK: Life Cycle
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+  }
+  
+  //MARK: Functions
+  
+  //MARK: Button Actions
+  
+  //MARK: Design Functions
+  
+
+ 
+
+}

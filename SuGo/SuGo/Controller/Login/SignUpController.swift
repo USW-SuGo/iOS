8976a649c49a7303ecfe6980566e58f610cb1901
sozohoy <sozohoy@gmail.com>
2022-12-22 20:46:06 +0900

    refactor: 회원가입 화면 if - else -> guard로 리펙토링, 유효성 bool값 메소드 활용하여 구분, api 호출 guard문 적용

--- SuGo/SuGo/Controller/Login/SignUpController.swift
+++ SuGo/SuGo/Controller/Login/SignUpController.swift
@@ -49,9 +49,6 @@ class SignUpController: UIViewController {
   var signUpIsValid: Bool {
       return emailIsValid && idIsValid && passwordIsValid && confirmPasswordIsValid
   }
-  
-  var keyboardTouchCheck = false
-  var emailTouch = false
   let colorLiteralGreen = #colorLiteral(red: 0.2208407819, green: 0.6479891539, blue: 0.4334517121, alpha: 1)
   let userInfo = UserInfo.shared
   
@@ -62,7 +59,7 @@ class SignUpController: UIViewController {
     customBackButton()
     customRightBarButton()
     designBox()
-    designLabel()
+    hiddenLabels()
     textFieldAddTargets()
     super.viewDidLoad()
   }
@@ -76,8 +73,8 @@ class SignUpController: UIViewController {
     confirmPasswordTextField.delegate = self
   }
 
+  // 텍스트필드에 action 추가
   private func textFieldAddTargets() {
-    // email add targets
     emailTextField.addTarget(self,
                              action: #selector(emailTextFieldEditingChanged),
                              for: .touchDown)
@@ -87,8 +84,6 @@ class SignUpController: UIViewController {
     emailTextField.addTarget(self,
                              action: #selector(emailTextFieldisValid),
                              for: .editingDidEnd)
-      
-    // id add targets
     idTextField.addTarget(self,
                           action: #selector(idTextFieldEditingChanged),
                           for: .touchDown)
@@ -98,8 +93,6 @@ class SignUpController: UIViewController {
     idTextField.addTarget(self,
                           action: #selector(idTextFieldisValid),
                           for: .editingDidEnd)
-    
-    // password add targets
     passwordTextField.addTarget(self,
                                 action: #selector(passwordTextFieldEditingChanged),
                                 for: .touchDown)
@@ -110,8 +103,6 @@ class SignUpController: UIViewController {
                                 action: #selector(passwordTextFieldisValid),
                                 for: .editingDidEnd)
     passwordTextField.isSecureTextEntry = true
-      
-    // confirm add targets
     confirmPasswordTextField.addTarget(self,
                                        action: #selector(confirmPasswordTextFieldEditingChanged),
                                        for: .touchDown)
@@ -131,161 +122,126 @@ class SignUpController: UIViewController {
   // 기존 입력마다 형식 알려주던 방식 대신, endEditing 이후 유효성 검사하는 방식으로 전환
   
   @objc func emailTextFieldEditingChanged(_ sender: UITextField) {
-      
-      emailTouch = true
-      
       emailBox.layer.borderColor = colorLiteralGreen.cgColor
       idBox.layer.borderColor = UIColor.lightGray.cgColor
       passwordBox.layer.borderColor = UIColor.lightGray.cgColor
       confirmPasswordBox.layer.borderColor = UIColor.lightGray.cgColor
-      
   }
   
 
   @objc func emailTextFieldisValid(_ sender: UITextField) {
-      
-      emailTouch = false
-      emailWarningLabel.isHidden = false
-      
-      guard let email = emailTextField.text, !email.isEmpty else { return }
-      
-      if loginModel.isValidEmail(email: email) {
-          
-          emailOverlapCheck(email: email + "@suwon.ac.kr")
-          
-      } else {
-          
-          emailIsValid = false
-          warningText(label: emailWarningLabel,
-                      box: emailBox,
-                      text: "이메일 형식이 올바르지 않아요.",
-                      textColor: UIColor.red,
-                      borderColor: UIColor.red.cgColor)
-          
-      }
-      
-}
+    emailWarningLabel.isHidden = false
+    guard let email = emailTextField.text, !email.isEmpty else { return }
+    guard loginModel.isValidEmail(email: email) else {
+      warningText(label: emailWarningLabel,
+                  box: emailBox,
+                  text: "이메일 형식이 올바르지 않아요.",
+                  textColor: UIColor.red,
+                  borderColor: UIColor.red.cgColor,
+                  validType: &emailIsValid,
+                  bool: false)
+      return
+    }
+    emailOverlapCheck(email: email + "@suwon.ac.kr")
+  }
   
   @objc func idTextFieldEditingChanged(_ sender: UITextField) {
-      
-      emailBox.layer.borderColor = UIColor.lightGray.cgColor
-      idBox.layer.borderColor = colorLiteralGreen.cgColor
-      passwordBox.layer.borderColor = UIColor.lightGray.cgColor
-      confirmPasswordBox.layer.borderColor = UIColor.lightGray.cgColor
-      
+    emailBox.layer.borderColor = UIColor.lightGray.cgColor
+    idBox.layer.borderColor = colorLiteralGreen.cgColor
+    passwordBox.layer.borderColor = UIColor.lightGray.cgColor
+    confirmPasswordBox.layer.borderColor = UIColor.lightGray.cgColor
   }
   
   @objc func idTextFieldisValid(_ sender: UITextField) { // id 정규표현식 확인
-      
-      idWarningLabel.isHidden = false
-      
-      guard let id = idTextField.text, !id.isEmpty else { return }
-      
-      //1. 아이디 형식 체크 후 일치하지 않을 경우 경고문 ON
-      if loginModel.isValidId(id: id) {
-          
-          idOverlapCheck(id: id)
-          
-      } else {
-          
-          warningText(label: idWarningLabel,
-                      box: idBox,
-                      text: "영문/숫자를 사용한 5~20자 아이디를 사용해주세요.",
-                      textColor: UIColor.red,
-                      borderColor: UIColor.red.cgColor)
-          idIsValid = false
-          
-      }
+    idWarningLabel.isHidden = false
+    guard let id = idTextField.text, !id.isEmpty else { return }
+    
+    //1. 아이디 형식 체크 후 일치하지 않을 경우 경고문 ON
+    guard loginModel.isValidId(id: id) else {
+      warningText(label: idWarningLabel,
+                  box: idBox,
+                  text: "영문/숫자를 사용한 5~20자 아이디를 사용해주세요.",
+                  textColor: UIColor.red,
+                  borderColor: UIColor.red.cgColor,
+                  validType: &idIsValid,
+                  bool: false)
+      return
+    }
+    idOverlapCheck(id: id)
   }
   
   @objc func passwordTextFieldEditingChanged(_ sender: UITextField) {
-      
-      emailBox.layer.borderColor = UIColor.lightGray.cgColor
-      idBox.layer.borderColor = UIColor.lightGray.cgColor
-      passwordBox.layer.borderColor = colorLiteralGreen.cgColor
-      confirmPasswordBox.layer.borderColor = UIColor.lightGray.cgColor
-      
+    emailBox.layer.borderColor = UIColor.lightGray.cgColor
+    idBox.layer.borderColor = UIColor.lightGray.cgColor
+    passwordBox.layer.borderColor = colorLiteralGreen.cgColor
+    confirmPasswordBox.layer.borderColor = UIColor.lightGray.cgColor
   }
   
   @objc func passwordTextFieldisValid(_ sender: UITextField) { // password 정규표현식
-      
-      passwordWarningLabel.isHidden = false
-      
-      guard let password = passwordTextField.text, !password.isEmpty else { return }
-      
-      // 정규표현식
-      
-      if loginModel.isValidPassword(pwd: password) {
-          
-          warningText(label: passwordWarningLabel,
-                      box: passwordBox,
-                      text: "사용 가능한 비밀번호에요!",
-                      textColor: colorLiteralGreen,
-                      borderColor: colorLiteralGreen.cgColor)
-          passwordIsValid = true
-          
-      } else {
-          
-//            passwordBox.layer.borderColor = UIColor.red.cgColor
-          warningText(label: passwordWarningLabel,
-                      box: passwordBox,
-                      text: "영문/숫자/특수문자를 조합한 8~20자 비밀번호를 사용해주세요.",
-                      textColor: UIColor.red,
-                      borderColor: UIColor.red.cgColor)
-          passwordIsValid = false
-      
-      }
-      
-      if confirmPasswordTextField.text?.count ?? 0 > 0 && password != confirmPasswordTextField.text {
-          // 2차 비밀번호 불일치 출력
-          warningText(label: confirmPasswordWarningLabel,
-                      box: confirmPasswordBox,
-                      text: "비밀번호가 일치하지 않아요.",
-                      textColor: UIColor.red,
-                      borderColor: UIColor.red.cgColor)
-          confirmPasswordIsValid = false
-          print("print : 2차 비밀번호까지 입력 후 1차 비밀번호 변경 시 불일치")
-          
-      }
-      
+    passwordWarningLabel.isHidden = false
+    
+    guard let password = passwordTextField.text, !password.isEmpty else { return }
+    guard loginModel.isValidPassword(pwd: password) else {
+      warningText(label: passwordWarningLabel,
+                  box: passwordBox,
+                  text: "영문/숫자/특수문자를 조합한 8~20자 비밀번호를 사용해주세요.",
+                  textColor: UIColor.red,
+                  borderColor: UIColor.red.cgColor,
+                  validType: &passwordIsValid,
+                  bool: false)
+      return
+    }
+    guard let count = confirmPasswordTextField.text?.count,
+              count > 0,
+              password == confirmPasswordTextField.text else {
+      warningText(label: confirmPasswordWarningLabel,
+                  box: confirmPasswordBox,
+                  text: "비밀번호가 일치하지 않아요.",
+                  textColor: UIColor.red,
+                  borderColor: UIColor.red.cgColor,
+                  validType: &confirmPasswordIsValid,
+                  bool: false)
+      return
+    }
+
+    warningText(label: passwordWarningLabel,
+                box: passwordBox,
+                text: "사용 가능한 비밀번호에요!",
+                textColor: colorLiteralGreen,
+                borderColor: colorLiteralGreen.cgColor,
+                validType: &passwordIsValid,
+                bool: true)
   }
   
   @objc func confirmPasswordTextFieldEditingChanged(_ sender: UITextField) {
-      
       emailBox.layer.borderColor = UIColor.lightGray.cgColor
       idBox.layer.borderColor = UIColor.lightGray.cgColor
       passwordBox.layer.borderColor = UIColor.lightGray.cgColor
       confirmPasswordBox.layer.borderColor = colorLiteralGreen.cgColor
-      
   }
   
   @objc func confirmPasswordTextFieldisValid(_ sender: UITextField) {
+    confirmPasswordWarningLabel.isHidden = false
       
-      confirmPasswordWarningLabel.isHidden = false
-      
-      guard let password = confirmPasswordTextField.text, !password.isEmpty else { return }
-
-      if password == passwordTextField.text { // 비밀번호 일치
-          
-          warningText(label: confirmPasswordWarningLabel,
-                      box: confirmPasswordBox,
-                      text: "비밀번호가 일치해요!",
-                      textColor: colorLiteralGreen,
-                      borderColor: colorLiteralGreen.cgColor)
-          confirmPasswordIsValid = true
-          
-          
-      } else { // 비밀번호 불일치
-          
-          warningText(label: confirmPasswordWarningLabel,
-                      box: confirmPasswordBox,
-                      text: "비밀번호가 일치하지 않아요.",
-                      textColor: UIColor.red,
-                      borderColor: UIColor.red.cgColor)
-          confirmPasswordIsValid = false
-          
-          
-      }
+    guard let password = confirmPasswordTextField.text, !password.isEmpty else { return }
+    guard password == passwordTextField.text else {
+      warningText(label: confirmPasswordWarningLabel,
+                  box: confirmPasswordBox,
+                  text: "비밀번호가 일치하지 않아요.",
+                  textColor: UIColor.red,
+                  borderColor: UIColor.red.cgColor,
+                  validType: &confirmPasswordIsValid,
+                  bool: false)
+      return
+    }
+    
+    warningText(label: confirmPasswordWarningLabel,
+                box: confirmPasswordBox,
+                text: "비밀번호가 일치해요!",
+                textColor: colorLiteralGreen,
+                borderColor: colorLiteralGreen.cgColor,
+                validType: &confirmPasswordIsValid,
+                bool: true)
   }
   
   @objc func closeButtonClicked() {
@@ -321,67 +277,67 @@ class SignUpController: UIViewController {
   //MARK: API Functions
   
   func emailOverlapCheck(email: String) {
-      AlamofireManager
-          .shared
-          .session
-          .request(LoginRouter.checkEmail(email: email))
-          .responseJSON { response in
-//                print(JSON(response.data))
-              if JSON(response.data ?? "")["exist"].boolValue == false {
-                  
-                  self.warningText(label: self.emailWarningLabel,
-                              box: self.emailBox,
-                              text: "사용 가능한 이메일이에요!",
-                              textColor: self.colorLiteralGreen,
-                              borderColor: self.colorLiteralGreen.cgColor)
-                  self.emailIsValid = true
-                  
-              } else {
-                  
-                  self.warningText(label: self.emailWarningLabel,
-                              box: self.emailBox,
-                              text: "이미 사용중이거나, 사용할 수 없는 이메일이에요.",
-                              textColor: UIColor.red,
-                              borderColor: UIColor.red.cgColor)
-                  self.emailIsValid = false
-                  
-              }
-          }
-
+    AlamofireManager
+      .shared
+      .session
+      .request(LoginRouter.checkEmail(email: email))
+      .responseJSON { response in
+        guard let statusCode = response.response?.statusCode, statusCode == 200 else {
+          return
+        }
+        guard let data = response.data else { return }
+        guard JSON(data)["exist"].boolValue else {
+          self.warningText(label: self.emailWarningLabel,
+                           box: self.emailBox,
+                           text: "이미 사용중이거나, 사용할 수 없는 이메일이에요.",
+                           textColor: UIColor.red,
+                           borderColor: UIColor.red.cgColor,
+                           validType: &self.emailIsValid,
+                           bool: false)
+          return
+        }
+        self.warningText(label: self.emailWarningLabel,
+                         box: self.emailBox,
+                         text: "사용 가능한 이메일이에요!",
+                         textColor: self.colorLiteralGreen,
+                         borderColor: self.colorLiteralGreen.cgColor,
+                         validType: &self.emailIsValid,
+                         bool: true)
+    }
   }
   
   func idOverlapCheck(id: String) {
-      AlamofireManager
-          .shared
-          .session
-          .request(LoginRouter.checkLoginId(id: id))
-          .responseJSON { response in
-              
-              if response.response?.statusCode == 200 {
-                  
-                  self.warningText(label: self.idWarningLabel,
-                              box: self.idBox,
-                              text: "사용 가능한 아이디에요!",
-                              textColor: self.colorLiteralGreen,
-                              borderColor: self.colorLiteralGreen.cgColor)
-                  self.idIsValid = true
-                
-              } else {
-                  
-                  self.warningText(label: self.idWarningLabel,
-                                   box: self.idBox,
-                                   text: "이미 사용중인 아이디에요!",
-                                   textColor: UIColor.red,
-                                   borderColor: UIColor.red.cgColor)
-                  self.idIsValid = false
-                  
-              }
-              
-          }
+    AlamofireManager
+      .shared
+      .session
+      .request(LoginRouter.checkLoginId(id: id))
+      .responseJSON { response in
+        guard let statusCode = response.response?.statusCode, statusCode == 200 else { return }
+        guard let data = response.data else { return }
+        guard JSON(data)["exist"].boolValue else {
+          self.warningText(label: self.idWarningLabel,
+                           box: self.idBox,
+                           text: "이미 사용중인 아이디에요!",
+                           textColor: UIColor.red,
+                           borderColor: UIColor.red.cgColor,
+                           validType: &self.idIsValid,
+                           bool: false)
+          return
+        }
+        self.idIsValid = true
+        self.warningText(label: self.idWarningLabel,
+                         box: self.idBox,
+                         text: "사용 가능한 아이디에요!",
+                         textColor: self.colorLiteralGreen,
+                         borderColor: self.colorLiteralGreen.cgColor,
+                         validType: &self.idIsValid,
+                         bool: true)
+    }
   }
   
-
-  
+  private func typeIsValid(validType: inout Bool, bool: Bool) {
+    validType = bool
+  }
   
   //MARK: Button Actions
    
@@ -398,21 +354,17 @@ class SignUpController: UIViewController {
         self.navigationController?.pushViewController(nextViewController, animated: true)
           
       } else {
-          
         customAlert(title: "계정 정보를 확인해주세요 !", message: "입력되지 않았거나, 올바르지 않은 정보가 있어요 !")
-          
       }
   }
 
   //MARK: Design Funtions
   
   private func customRightBarButton() {
-      
     let closeButton = self.navigationItem.makeSFSymbolButton(self,
                                                              action: #selector(closeButtonClicked),
                                                              symbolName: "xmark")
     self.navigationItem.rightBarButtonItem = closeButton
-
   }
     
   private func customAlert(title: String, message: String) {
@@ -422,36 +374,35 @@ class SignUpController: UIViewController {
   }
   
   private func warningText(label: UILabel,
-                   box: UIView,
-                   text: String,
-                   textColor: UIColor,
-                   borderColor: CGColor) {
+                           box: UIView,
+                           text: String,
+                           textColor: UIColor,
+                           borderColor: CGColor,
+                           validType: inout Bool,
+                           bool: Bool) {
     label.text = text
     label.textColor = textColor
     box.layer.borderColor = borderColor
+    typeIsValid(validType: &validType, bool: bool)
   }
-
+  
   func designBox() {
     emailBox.layer.borderWidth = 0.7
     emailBox.layer.borderColor = UIColor.lightGray.cgColor
     emailBox.layer.cornerRadius = 3.0
-    
     idBox.layer.borderWidth = 0.7
     idBox.layer.borderColor = UIColor.lightGray.cgColor
     idBox.layer.cornerRadius = 3.0
-    
     passwordBox.layer.borderWidth = 0.7
     passwordBox.layer.borderColor = UIColor.lightGray.cgColor
     passwordBox.layer.cornerRadius = 3.0
-    
     confirmPasswordBox.layer.borderWidth = 0.7
     confirmPasswordBox.layer.borderColor = UIColor.lightGray.cgColor
     confirmPasswordBox.layer.cornerRadius = 3.0
-    
     nextButton.layer.cornerRadius = 6.0
   }
   
-  func designLabel() {
+  func hiddenLabels() {
     emailWarningLabel.isHidden = true
     idWarningLabel.isHidden = true
     passwordWarningLabel.isHidden = true
@@ -459,11 +410,11 @@ class SignUpController: UIViewController {
   }
     
   private func customBackButton() {
-      let backButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
-      backButtonItem.tintColor = .darkGray
-      self.navigationItem.backBarButtonItem = backButtonItem
+    let backButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
+    backButtonItem.tintColor = .darkGray
+    self.navigationItem.backBarButtonItem = backButtonItem
   }
-    
+  
 }
 
 extension SignUpController: UITextFieldDelegate {

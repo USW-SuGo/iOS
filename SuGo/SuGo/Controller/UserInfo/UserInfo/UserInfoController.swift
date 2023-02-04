ed3a2fae573818591b4fa591006149892dc88f2a
sozohoy <sozohoy@gmail.com>
2023-02-04 20:05:02 +0900

    feat: 유저 페이지 api 통신 테스트

--- SuGo/SuGo/Controller/UserInfo/UserInfo/UserInfoController.swift
+++ SuGo/SuGo/Controller/UserInfo/UserInfo/UserInfoController.swift
@@ -7,12 +7,40 @@
 
 import UIKit
 
+import Alamofire
+import SwiftyJSON
+
 class UserInfoController: UIViewController {
 
+  //MARK: Properties
+  
+  var userId: Int?
+  
+  //MARK: Life Cycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
-      // Do any additional setup after loading the view.
+    if let userId = userId {
+      getUserPage(userId: userId, page: 0, size: 10)
+    }
+    print("userId: \(userId ?? 1123131313)")
+  }
+  
+  //MARK: Functions
+ 
+  //MARK: API Functions
+  
+  private func getUserPage(userId: Int, page: Int, size: Int) {
+    AlamofireManager
+      .shared
+      .session
+      .request(PageRouter.userPage(userId: userId, page: page, size: size))
+      .validate()
+      .response { response in
+        print(JSON(response.data ?? ""))
+      }
+        
+      
   }
   
 

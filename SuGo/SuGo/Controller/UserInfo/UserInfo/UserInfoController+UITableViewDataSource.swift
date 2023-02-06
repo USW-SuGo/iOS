ebd98f133704d82ce04f81d29791c9a4856940f2
sozohoy <sozohoy@gmail.com>
2023-02-06 19:59:34 +0900

    feat: 판매 완료 API 불러오기 스케치

--- SuGo/SuGo/Controller/UserInfo/UserInfo/UserInfoController+UITableViewDataSource.swift
+++ SuGo/SuGo/Controller/UserInfo/UserInfo/UserInfoController+UITableViewDataSource.swift
@@ -10,31 +10,66 @@ import UIKit
 
 extension UserInfoController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return userSalePosting.count
+    switch tableView.tag {
+    case 1:
+      return userSalePosting.count
+    case 2:
+      return userSoldOutPosting.count
+    default: return 1
+    }
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "userPostingCell",
-                                                    for: indexPath) as! UserPostingCell
-    if userSalePosting.count > 0 {
-      if let url = URL(string: userSalePosting[indexPath.row].imageLink) {
-        cell.productImage.kf.indicatorType = .activity
-        cell.productImage.kf.setImage(with: url,
-                                      placeholder: nil,
-                                      options: [
-                                        .transition(.fade(0.1)),
-                                        .cacheOriginalImage
-                                      ],
-                                      progressBlock: nil)
+    switch tableView.tag {
+    case 1:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "userPostingCell",
+                                                      for: indexPath) as! UserPostingCell
+      if userSalePosting.count > 0 {
+        if let url = URL(string: userSalePosting[indexPath.row].imageLink) {
+          cell.productImage.kf.indicatorType = .activity
+          cell.productImage.kf.setImage(with: url,
+                                        placeholder: nil,
+                                        options: [
+                                          .transition(.fade(0.1)),
+                                          .cacheOriginalImage
+                                        ],
+                                        progressBlock: nil)
+        }
+        cell.productImage.contentMode = .scaleAspectFill
+        cell.productImage.layer.cornerRadius = 6.0
+        cell.placeUpdateCategoryLabel.text =
+        "\(userSalePosting[indexPath.row].contactPlace) | \(userSalePosting[indexPath.row].updatedAt) | \(userSalePosting[indexPath.row].category)"
+        cell.titleLabel.text = userSalePosting[indexPath.row].title
+        cell.priceLabel.text = userSalePosting[indexPath.row].price
+        cell.selectionStyle = .none
       }
-      cell.productImage.contentMode = .scaleAspectFill
-      cell.productImage.layer.cornerRadius = 6.0
-      cell.placeUpdateCategoryLabel.text =
-      "\(userSalePosting[indexPath.row].contactPlace) | \(userSalePosting[indexPath.row].updatedAt) | \(userSalePosting[indexPath.row].category)"
-      cell.titleLabel.text = userSalePosting[indexPath.row].title
-      cell.priceLabel.text = userSalePosting[indexPath.row].price
-      cell.selectionStyle = .none
+      return cell
+    case 2:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "userPostingCell",
+                                                      for: indexPath) as! UserPostingCell
+      if userSoldOutPosting.count > 0 {
+        if let url = URL(string: userSoldOutPosting[indexPath.row].imageLink) {
+          cell.productImage.kf.indicatorType = .activity
+          cell.productImage.kf.setImage(with: url,
+                                        placeholder: nil,
+                                        options: [
+                                          .transition(.fade(0.1)),
+                                          .cacheOriginalImage
+                                        ],
+                                        progressBlock: nil)
+        }
+        cell.productImage.contentMode = .scaleAspectFill
+        cell.productImage.layer.cornerRadius = 6.0
+        cell.placeUpdateCategoryLabel.text =
+        "\(userSoldOutPosting[indexPath.row].contactPlace) | \(userSoldOutPosting[indexPath.row].updatedAt) | \(userSoldOutPosting[indexPath.row].category)"
+        cell.titleLabel.text = userSoldOutPosting[indexPath.row].title
+        cell.priceLabel.text = userSoldOutPosting[indexPath.row].price
+        cell.selectionStyle = .none
+      }
+      return cell
+    default:
+      return UITableViewCell()
     }
-    return cell
+    
   }
 }
--- SuGo/SuGo/Controller/UserInfo/UserInfo/UserInfoController+UITableViewDelegate.swift
+++ SuGo/SuGo/Controller/UserInfo/UserInfo/UserInfoController+UITableViewDelegate.swift
@@ -9,17 +9,33 @@ import Foundation
 import UIKit
 
 extension UserInfoController: UITableViewDelegate {
-  
+  // Swtich tableView.tag = 1(판매 중인 글 O), 2(판매 완료 글 O), 3(판매 중인 글 X), 4(판매 완료 글 X)
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     return 138
   }
   
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    switch tableView.tag {
+    case 1:
+      let postView = UIStoryboard(name: "PostView", bundle: nil)
+      guard let postController = postView.instantiateViewController(withIdentifier: "postVC")
+              as? PostController
+      else { return }
+      postController.productPostId = userSalePosting[indexPath.row].productIndex
+      self.navigationController?.pushViewController(postController, animated: true)
+    default: return
+    }
+    
+  }
+  
   func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-    let lastIndex = userSalePosting.count - 2
-    guard let userId = userId,
-            indexPath.row == lastIndex,
-            !salePostingLastPage else { return }
-    salePostingPage += 1
-    getUserPage(userId: userId, page: salePostingPage, size: 10)
+    if tableView.tag == 1 {
+      let lastIndex = userSalePosting.count - 2
+      guard let userId = userId,
+              indexPath.row == lastIndex,
+              !salePostingLastPage else { return }
+      salePostingPage += 1
+      getUserPage(userId: userId, page: salePostingPage, size: 10)
+    }
   }
 }

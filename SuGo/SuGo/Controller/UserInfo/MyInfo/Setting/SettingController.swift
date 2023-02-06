c0660d1360b2ee2fe677cf9f77c912dee2bb6d82
sozohoy <sozohoy@gmail.com>
2023-02-06 20:02:21 +0900

    feat: 설정 Controller 기능 구현 - 비밀번호 변경, 로그아웃

--- SuGo/SuGo/Controller/UserInfo/MyInfo/Setting/SettingController.swift
+++ SuGo/SuGo/Controller/UserInfo/MyInfo/Setting/SettingController.swift
@@ -7,23 +7,93 @@
 
 import UIKit
 
+import KeychainSwift
+
 class SettingController: UIViewController {
+  
+  //MARK: IBOutlets
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  //MARK: Properties
+  
+  let settingModel = SettingModel()
+  
+  //MARK: Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    customBackButton()
+      // Do any additional setup after loading the view.
+  }
+  
+  private func customBackButton() {
+    let backButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
+    backButtonItem.tintColor = .darkGray
+    self.navigationItem.backBarButtonItem = backButtonItem
+  }
+  
+  private func customAlert(title: String, message: String) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "확인", style: .default))
+    self.present(alert, animated: true, completion: nil)
+  }
+  
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+extension SettingController: UITableViewDelegate {
+  
+  // 비밀번호 변경 = section, row -> [1, 1]
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    switch indexPath{
+    case [1, 0]: // 비밀번호 변경
+      let changePasswordView = UIStoryboard(name: "ChangePasswordView", bundle: nil)
+      guard let changePasswordController =
+              changePasswordView.instantiateViewController(withIdentifier: "changePasswordVC")
+              as? ChangePasswordController
+      else { return }
+      self.navigationController?.pushViewController(changePasswordController, animated: true)
+    case [1, 2]: // 로그아웃
+      customAlert(title: "로그아웃 하시겠어요?", message: "로그아웃을 하시려면 확인 버튼을 눌러주세요!")
+//      KeychainSwift().clear()
+//      self.navigationController?.popViewController(animated: true)
+//      if let tabBarController = self.tabBarController {
+//        tabBarController.selectedIndex = 0
+//      }
+    default:
+      return
     }
-    
+  }
+  
+}
 
-    /*
-    // MARK: - Navigation
+extension SettingController: UITableViewDataSource {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return settingModel.menuCategories.count
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return settingModel.loginMenus[section].count
+  }
+  
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return settingModel.menuCategories[section]
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell",
+                                                   for: indexPath) as? SettingCell
+    else { return UITableViewCell() }
+    cell.testLabel.text = settingModel.loginMenus[indexPath.section][indexPath.row]
+    return cell
+  }
+  
+}
 
+class SettingCell: UITableViewCell {
+  
+  @IBOutlet weak var testLabel: UILabel!
+  
 }

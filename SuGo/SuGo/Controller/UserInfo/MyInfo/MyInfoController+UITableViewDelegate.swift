6b897581ce0b8a41911232d46c758a39f1e18d6a
sozohoy <sozohoy@gmail.com>
2023-02-06 20:00:31 +0900

    feat: 내가쓴 글 & 좋아요 누른 글 분기처리

--- SuGo/SuGo/Controller/UserInfo/MyInfo/MyInfoController+UITableViewDelegate.swift
+++ SuGo/SuGo/Controller/UserInfo/MyInfo/MyInfoController+UITableViewDelegate.swift
@@ -24,6 +24,23 @@ extension MyInfoController: UITableViewDelegate {
     }
   }
   
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let postView = UIStoryboard(name: "PostView", bundle: nil)
+    guard let postController = postView.instantiateViewController(withIdentifier: "postVC")
+            as? PostController else { return }
+    
+    switch tableView.tag {
+    case 1:
+      postController.productPostId = userPosting[indexPath.row].productIndex
+    case 2:
+      postController.productPostId = userLikePosting[indexPath.row].productIndex
+    default:
+      return
+    }
+    
+    navigationController?.pushViewController(postController, animated: true)
+  }
+  
   func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
     if tableView.tag == 1 {
       let lastIndex = userPosting.count - 2
@@ -37,27 +54,11 @@ extension MyInfoController: UITableViewDelegate {
     } else if tableView.tag == 2 {
       let lastIndex = userLikePosting.count - 2
       if indexPath.row == lastIndex {
-//        userLikePostingPage += 1
-//        if !userLikePostingLastPage {
-//          getMyPage(page: userLikePostingPage, size: 10, posting: "likePosting") }
+        userLikePostingPage += 1
+        if !userLikePostingLastPage {
+          getMyPage(page: userLikePostingPage, size: 10, posting: "likePostings") }
         }
       }
     }
-  
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    if tableView.tag == 1 {
-      let postView = UIStoryboard(name: "PostView", bundle: nil)
-      guard let postController = postView.instantiateViewController(withIdentifier: "postVC")
-              as? PostController else { return }
-      postController.productPostId = userPosting[indexPath.row].productIndex
-      navigationController?.pushViewController(postController, animated: true)
-    } else if tableView.tag == 2 {
-      let postView = UIStoryboard(name: "PostView", bundle: nil)
-      guard let postController = postView.instantiateViewController(withIdentifier: "postVC")
-              as? PostController else { return }
-      postController.productPostId = userLikePosting[indexPath.row].productIndex
-      navigationController?.pushViewController(postController, animated: true)
-    }
-  }
   
 }

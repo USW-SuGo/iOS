1b6ffe2e1dd26d123735cb8d9d3824247673d37f
sozohoy <sozohoy@gmail.com>
2023-02-21 21:27:38 +0900

    feat: FCM 사용을 위한 코드 작성

--- SuGo/SuGo/Resources/AppDelegate.swift
+++ SuGo/SuGo/Resources/AppDelegate.swift
@@ -7,10 +7,13 @@
 
 import UIKit
 
+import Firebase
+import FirebaseMessaging
+import UserNotifications
 import IQKeyboardManagerSwift
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
 
     let colorLiteralGreen = #colorLiteral(red: 0.2208407819, green: 0.6479891539, blue: 0.4334517121, alpha: 1)
 
@@ -19,11 +22,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       IQKeyboardManager.shared.enable = true
       IQKeyboardManager.shared.enableAutoToolbar = true
       IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+      FirebaseApp.configure()
+      
+      if #available(iOS 10.0, *) {
+        // For iOS 10 display notification (sent via APNS)
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: {_, _ in })
+      } else {
+        let settings: UIUserNotificationSettings =
+        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+        application.registerUserNotificationSettings(settings)
+      }
+
+      application.registerForRemoteNotifications()
+
+      Messaging.messaging().delegate = self
+
+      Messaging.messaging().token { token, error in
+        if let error = error {
+          print("Error fetching FCM registration token: \(error)")
+        } else if let token = token {
+          print("FCM registration token: \(token)")
+          
+        }
+      }
       
       return true
     }
+  
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("Firebase registration token: \(String(describing: fcmToken))")
+
+    
+    let dataDict:[String: String] = ["token": fcmToken ?? ""]
+    NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+    // TODO: If necessary send token to application server.
+    // Note: This callback is fired at each app startup and whenever a new token is generated.
+  }
 
     // MARK: UISceneSession Lifecycle
+  
+  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Messaging.messaging().apnsToken = deviceToken
+  }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.

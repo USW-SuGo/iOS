a4b5d87bb4e639862f020927361bcf562c5a8d2f
sozohoy <sozohoy@gmail.com>
2023-02-24 21:13:59 +0900

    feat: 유저 평가 case 추가

--- SuGo/SuGo/Network/PageRouter.swift
+++ SuGo/SuGo/Network/PageRouter.swift
@@ -12,59 +12,71 @@ import KeychainSwift
 
 enum PageRouter: URLRequestConvertible {
         
-    case myPage
-    case userPage(userId: Int)
+  case myPage
+  case userPage(userId: Int)
+  case userMannerEvaluate(userId: Int, grade: Double)
 
-    var baseURL: URL {
-        return URL(string: API.BASE_URL)!
-    }
+  var baseURL: URL {
+      return URL(string: API.BASE_URL + "/user")!
+  }
 
-    var method: HTTPMethod {
-        switch self {
-        case .myPage, .userPage:
-            return .get
-            
-        }
-    }
+  var method: HTTPMethod {
+      switch self {
+      case .myPage, .userPage:
+        return .get
+      case .userMannerEvaluate:
+        return .post
+      }
+  }
 
-    var path: String {
-        switch self {
-        case .myPage:
-            return "/user"
-        case .userPage(let userId):
-            return "/user/\(userId)/"
-        }
+  var path: String {
+    switch self {
+    case .myPage:
+      return ""
+    case .userPage(let userId):
+      return "/\(userId)/"
+    case .userMannerEvaluate:
+      return "/manner"
+    }
+  }
+  
+  var parameters: Parameters {
+    switch self {
+    case .myPage, .userPage : return [:]
+    case .userMannerEvaluate(let userId, let grade):
+      return [
+        "targetUserId" : userId,
+        "grade" : grade
+      ]
+    }
+  }
+  
+  var headers: HTTPHeaders {
+    switch self{
+    case .myPage, .userPage, .userMannerEvaluate:
+        return [
+            .authorization(String(KeychainSwift().get("AccessToken") ?? ""))
+        ]
+    }
+  }
+  
+  func asURLRequest() throws -> URLRequest {
+    let url = baseURL.appendingPathComponent(path) // string을 붙히는, path를 이어 붙힌다.
+    
+    print("LoginRouter - asURLRequest() url : \(url)")
+    
+    var request = URLRequest(url: url)
+    
+    request.method = method
+    request.headers = headers
+    
+    if request.method == .post {
+      request = try JSONEncoding.default.encode(request, with: parameters)
+    } else if request.method == .get {
+      request = try URLEncoding.default.encode(request, with: parameters)
     }
     
-    var parameters: Parameters {
-        switch self {
-        case .myPage, .userPage : return [:]
-       
-        }
-    }
-    
-    var headers: HTTPHeaders {
-        switch self{
-        case .myPage, .userPage:
-            return [
-                .authorization(String(KeychainSwift().get("AccessToken") ?? ""))
-            ]
-        }
-    }
-    
-    func asURLRequest() throws -> URLRequest {
-        let url = baseURL.appendingPathComponent(path) // string을 붙히는, path를 이어 붙힌다.
-        
-        print("LoginRouter - asURLRequest() url : \(url)")
-        
-        var request = URLRequest(url: url)
-        
-        request.method = method
-        request.headers = headers
-        request = try URLEncoding.default.encode(request, with: parameters)
-    
-        return request
-    }
-    
-    
+    return request
+  }
+  
 }

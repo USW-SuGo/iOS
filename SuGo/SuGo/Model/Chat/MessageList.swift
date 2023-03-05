220a628b051150b986d8708b795b78610495c7ba
sozohoy <sozohoy@gmail.com>
2023-03-05 20:10:30 +0900

    feat: messageList 구조체 구현(생성자, 초기화 위한 메소드)

--- SuGo/SuGo/Model/Chat/MessageList.swift
+++ SuGo/SuGo/Model/Chat/MessageList.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+import SwiftyJSON
+
 struct MessageList {
   var roomIndex: Int = 0
   var myIndex: Int = 0
@@ -16,6 +18,41 @@ struct MessageList {
   var recentMessage: String = ""
   var recentMessageTime: String = ""
   var newMessageCount: Int = 0
+  
+  init() { }
+  
+  init(json: JSON, requestUserId: Int) {
+    self.roomIndex = json["noteId"].intValue
+    let (myIndex, oppositeIndex) = checkIndex(requestUserId: requestUserId,
+                                              creatingUserId: json["creatingUserId"].intValue,
+                                              opponentUserId: json["opponentUserId"].intValue)
+    self.myIndex = myIndex
+    self.oppositeIndex = oppositeIndex
+    self.oppositeNickname = json["opponentUserNickname"].stringValue
+    self.productIndex = json["productPostId"].intValue
+    self.recentMessage = json["recentContent"].stringValue
+    self.recentMessageTime = dateToString(localDateTime: json)
+    self.newMessageCount = json["requestUserUnreadCount"].intValue
+  }
+  
+  private func checkIndex(requestUserId: Int,
+                          creatingUserId: Int,
+                          opponentUserId: Int) -> (Int, Int) {
+    let (myIndex, oppositeIndex) = requestUserId == opponentUserId
+    ? (opponentUserId, creatingUserId) : (creatingUserId, opponentUserId)
+    return (myIndex, oppositeIndex)
+  }
+  
+  private func dateToString(localDateTime: JSON) -> String {
+    let localDateTime = localDateTime["recentChattingDate"].stringValue
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+    let convertLocalToDate = dateFormatter.date(from: localDateTime) ?? Date()
+    dateFormatter.dateFormat = "MM-dd HH:mm"
+    let recentMessageTime = dateFormatter.string(from: convertLocalToDate)
+    print(recentMessageTime)
+    
+    return recentMessageTime
+  }
+  
 }
-
-

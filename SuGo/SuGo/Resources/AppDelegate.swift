28e629f6ab5ee2edfa732051d5f254e78056ea52
sozohoy <sozohoy@gmail.com>
2023-03-07 22:05:34 +0900

    feat: FCM 토큰을 UserDefault에 저장하도록 구현

--- SuGo/SuGo/Resources/AppDelegate.swift
+++ SuGo/SuGo/Resources/AppDelegate.swift
@@ -7,81 +7,84 @@
 
 import UIKit
 
+
 import Firebase
 import FirebaseMessaging
-import UserNotifications
 import IQKeyboardManagerSwift
+import KeychainSwift
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+  let keychain = KeychainSwift()
+  let colorLiteralGreen = #colorLiteral(red: 0.2208407819, green: 0.6479891539, blue: 0.4334517121, alpha: 1)
 
-    let colorLiteralGreen = #colorLiteral(red: 0.2208407819, green: 0.6479891539, blue: 0.4334517121, alpha: 1)
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-      
-      IQKeyboardManager.shared.enable = true
-      IQKeyboardManager.shared.enableAutoToolbar = true
-      IQKeyboardManager.shared.shouldResignOnTouchOutside = true
-      FirebaseApp.configure()
-      
-      if #available(iOS 10.0, *) {
-        // For iOS 10 display notification (sent via APNS)
-        UNUserNotificationCenter.current().delegate = self
-
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        UNUserNotificationCenter.current().requestAuthorization(
-          options: authOptions,
-          completionHandler: {_, _ in })
-      } else {
-        let settings: UIUserNotificationSettings =
-        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-        application.registerUserNotificationSettings(settings)
-      }
-
-      application.registerForRemoteNotifications()
-
-      Messaging.messaging().delegate = self
-
-      Messaging.messaging().token { token, error in
-        if let error = error {
-          print("Error fetching FCM registration token: \(error)")
-        } else if let token = token {
-          print("FCM registration token: \(token)")
-          
-        }
-      }
-      
-      return true
-    }
-  
-  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-    print("Firebase registration token: \(String(describing: fcmToken))")
-
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
-    let dataDict:[String: String] = ["token": fcmToken ?? ""]
+    IQKeyboardManager.shared.enable = true
+    IQKeyboardManager.shared.enableAutoToolbar = true
+    IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+    FirebaseApp.configure()
+    
+    if #available(iOS 10.0, *) {
+      // For iOS 10 display notification (sent via APNS)
+      UNUserNotificationCenter.current().delegate = self
+
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: {_, _ in })
+    } else {
+      let settings: UIUserNotificationSettings =
+      UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
+    }
+
+    application.registerForRemoteNotifications()
+
+    Messaging.messaging().delegate = self
+
+    Messaging.messaging().token { token, error in
+      if let error = error {
+        print("Error fetching FCM registration token: \(error)")
+      } else if let token = token {
+        print("FCM registration token: \(token)")
+        UserDefaults.standard.set(token, forKey: "FCMToken")
+      }
+    }
+    
+    return true
+  }
+
+
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    guard let token = fcmToken else {
+      print("FCM TOKEN ERROR OCCURED!")
+      return }
+    let dataDict: [String: String] = ["token" : token]
     NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
     // TODO: If necessary send token to application server.
     // Note: This callback is fired at each app startup and whenever a new token is generated.
   }
 
-    // MARK: UISceneSession Lifecycle
-  
+  // MARK: UISceneSession Lifecycle
+
   func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
     Messaging.messaging().apnsToken = deviceToken
   }
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+      // Called when a new scene session is being created.
+      // Use this method to select a configuration to create the new scene with.
+      return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+      // Called when the user discards a scene session.
+      // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+      // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+  }
 
 
-}
+  }
 
